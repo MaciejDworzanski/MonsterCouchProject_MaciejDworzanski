6ddc8b93688089d4bc54131824c5545dbf89e70c
Maciej Dworzański <maciejdtme@gmail.com>
2025-11-03 19:25:14 +0100

    Added way to quit game scene to menu

--- Assets/_Project/Scripts/Player/PlayerInput.cs
+++ Assets/_Project/Scripts/Player/PlayerInput.cs
@@ -6,10 +6,14 @@ namespace _Project.Scripts.Player
     public class PlayerInput : MonoBehaviour
     {
         public event Action<Vector2> OnPlayerMove;
+
+        public static event Action OnPlayerWantsToExit;
         
         private void FixedUpdate()
         {
             HandleMovement();
+            if(Input.GetKeyDown(KeyCode.Escape))
+                OnPlayerWantsToExit?.Invoke();
         }
 
         private void HandleMovement()
---
+++ Assets/_Project/Scripts/SceneControllers/MainGameSceneController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using _Project.Scripts.Player;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace _Project.Scripts.SceneControllers
+{
+    public class MainGameSceneController : MonoBehaviour
+    {
+        private void OnEnable()
+        {
+            PlayerInput.OnPlayerWantsToExit += QuitScene;
+        }
+
+        private void OnDisable()
+        {
+            PlayerInput.OnPlayerWantsToExit -= QuitScene;
+        }
+
+        private void QuitScene()
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
