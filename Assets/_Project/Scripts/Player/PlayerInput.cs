fa41ae3934288caa686fd0d99fae652f7e6898de
Maciej Dworzański <maciejdtme@gmail.com>
2025-11-03 18:55:30 +0100

    Added second scene, added some basic player input handling

---
+++ Assets/_Project/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEngine;
+
+namespace _Project.Scripts.Player
+{
+    public class PlayerInput : MonoBehaviour
+    {
+        public event Action<Vector2> OnPlayerMove; 
+        private void Update()
+        {
+            HandleMovement();
+        }
+
+        private void HandleMovement()
+        {
+            var moveX = Input.GetAxis("Horizontal");
+            var moveY = Input.GetAxis("Vertical");
+            var movementVector = new Vector2(moveX, moveY);
+            OnPlayerMove?.Invoke(movementVector);
+        }
+    }
+}
