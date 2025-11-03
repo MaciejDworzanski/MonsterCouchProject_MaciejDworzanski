774ae0b345a56c85a6548995d418ca1bf5366007
Maciej Dworzański <maciejdtme@gmail.com>
2025-11-03 17:57:37 +0100

    Added base scripts for window and UI manager

---
+++ Assets/_Project/Scripts/UI/MainMenuWindow.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace _Project.Scripts.UI
+{
+    public class MainMenuWindow : WindowBase
+    {
+        [SerializeField]
+        private Button playButton, settingsButton, exitButton;
+    }
+}
---
+++ Assets/_Project/Scripts/UI/WindowBase.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace _Project.Scripts.UI
+{
+    public abstract class WindowBase : MonoBehaviour
+    {
+        public static event Action<WindowBase> OnOpen, OnClose;
+
+        [SerializeField]
+        private GameObject content;
+
+        [SerializeField]
+        private bool OpenOnStart;
+
+        [SerializeField]
+        private Selectable defaultSelectable;
+
+        private void Start()
+        {
+            if (OpenOnStart)
+                Open();
+            else
+                Close();
+        }
+
+        public void Open()
+        {
+            if (!CanOpen())
+                return;
+            content.SetActive(true);
+            OnOpen?.Invoke(this);
+            SelectDefaultOrLastSelectedButton();
+        }
+
+        public void Close()
+        {
+            content.SetActive(false);
+            OnClose?.Invoke(this);
+        }
+
+        private void SelectDefaultOrLastSelectedButton()
+        {
+            EventSystem.current.SetSelectedGameObject(defaultSelectable.gameObject);
+        }
+
+        protected virtual bool CanOpen()
+        {
+            return true;
+        }
+    }
+}
